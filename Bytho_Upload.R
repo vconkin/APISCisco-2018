bb259023e06af12be2c2572aa108a88d26c7c954
TaylorStewart <taylor.stewart@uvm.edu>
2019-06-07 09:57:51 -0400

    simplified ZEMS conversion process significantly

--- Bytho_Upload.R
+++ Bytho_Upload.R
@@ -19,59 +19,50 @@ rm(list = ls(all = TRUE))
 
 
 ## ===========================================================
-## 
+## Load Packages =============================================
 ## ===========================================================
-# Make a list of all the files in the folder
-datafiles <- dir(path = "data/Superior_Files/Bytho", pattern="*.csv", full.names = TRUE)
+library(dplyr)         # manipulating data
+library(data.table)    # for fread() - super fast load-in of data
 
-# Make an empty vector where you can put the names of each file
-filelist <- rep(NA, length(datafiles))
+## ===========================================================
+## Step 1: Load in the data ==================================
+## ===========================================================
+## -----------------------------------------------------------
+## Notes:
+## Read in all the files you want and combine into one long merged file.
+## Use the loop to read in all the pooled files.
+## Make sure your directory is set to the lowest folder in the path with the files.
+## -----------------------------------------------------------
 
-# Now use your list to read in each data file
-for (i in 1:length(datafiles)){
+## -----------------------------------------------------------
+## Make a list of all the files in the folder for loop to read
+## -----------------------------------------------------------
+datafiles <- list.files(path = "data/Superior_Files/Bytho", pattern = ".csv", full.names = TRUE)
+
+## -----------------------------------------------------------
+## Now use your list to read in each data file
+## -----------------------------------------------------------
+summaryfile <- do.call(rbind, lapply(datafiles, function(i) {
+  ## print filename in console
+  print(i)
   
-  # Save the name of the filepath so you can reference it later 
-  filename <- paste(datafiles[i])
-  
-  # Also save the filename without the .csv extension
-  dataframename <- substr(paste(datafiles[i]), 1, nchar(paste(datafiles[i]))-4)
-  
-  # Record the name of the data file 
-  filelist[i] <- dataframename
-  
-  # Read in data 
-  freqdata <- read.table(filename, sep=",", header=TRUE)
-  
-  # Add columns at the end with lake name (which is in the filename)
-  #tempstring <- unlist(strsplit(dataframename, split="_", fixed = FALSE, perl = FALSE, useBytes = FALSE))
-  #freqdata$Lake <- tempstring[2]
-  
-  # Assign the data that you have pulled out to the filename where you got it
-  assign(dataframename,freqdata)
-  
-  if(i==1){ # If you're looking at the first raw data file, start a new data frame 
-    # to store all of your data
-    summaryfile <- freqdata
-  } else{ # Or else add to the data frame you already started
-    summaryfile <- rbind(summaryfile,freqdata)
-  }
-}
+  ## load data and combine into data frame using do.call()
+  fread(file = i)
+}))
 
 
-#-----------------------------------------------------------------------------------# 
-# Step 2: Fill in column for subsample expansion coefficient. Keep only columns with 
-# data stored in them and get rid of blank/unused columns to clean up a bit. Then export 
-# merged file to working dir.
+## ===========================================================
+# Step 2: Fill in columns for subsample expansion coefficient.
+## ===========================================================
+bytho.samples <- summaryfile %>% 
+  mutate(subSampleExpansionCoef = subSampleTotal / amountSubSampled) %>% 
+  ## Select (keep) only columns with data
+  select(subSampleID, amountSubSampled, subSampleTotal, subSampleExpansionCoef, species, organismCount, length)
 
 
-summaryfile$subSampleExpansionCoef= summaryfile$subSampleTotal / summaryfile$amountSubSampled
+## ===========================================================
+## Step 3: Save merged file to working dir.
+## ===========================================================
+write.csv(bytho.samples, "data/Superior_Files/Summaries/Bytho_Samples.csv", row.names = FALSE)
 
-Bytho.Samples <- subset(summaryfile, select = c(subSampleID,
-                                              amountSubSampled, 
-                                              subSampleTotal,
-                                              subSampleExpansionCoef,
-                                              species,
-                                              organismCount,
-                                              length))
-write.csv(Bytho.Samples, "data/Superior_Files/Summaries/Bytho.Samples.csv", row.names = FALSE)
-
+#----------------------------------------END----------------------------------------
