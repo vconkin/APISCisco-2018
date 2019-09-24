2e41cb856873f1fce34f91d6c26871d87f53be88
TaylorStewart <taylor.stewart@uvm.edu>
2019-09-24 09:46:13 -0400

    update length freq figure

--- Length_Freq.R
+++ Length_Freq.R
@@ -30,22 +30,12 @@ larval.tl <- read_excel("data/APIS_Coregonus_2018.xlsx", sheet = "Larval_Length_
 ## Calculate weekly length frequency for each length bin =====
 
 larval.tl.freq <- larval.tl %>% group_by(week, tl.bin) %>% 
-  summarize(n.tl = n(),
-            mean.tl = mean(tl.mm)) %>% 
-  ungroup()
-
-## Calculate freq for each tl bin and yolk-sac condition =====
-
-larval.yolk.freq <- larval.tl %>% group_by(tl.bin, yolk.cond) %>% 
   summarize(n.tl = n()) %>% 
+  ungroup() %>% 
+  group_by(week) %>% 
+  mutate(n.week = sum(n.tl)) %>% 
   ungroup()
 
-## Rename yolk-sac conditions
-
-larval.yolk.freq$yolk.cond <- gsub("Yolk sac and globule", "Yolk Sac Present", larval.yolk.freq$yolk.cond)
-larval.yolk.freq$yolk.cond <- gsub('Oil globule only', 'Oil Globule Only', larval.yolk.freq$yolk.cond)
-
-larval.yolk.freq %<>% mutate(yolk.cond = factor(yolk.cond, ordered = TRUE, levels = c('Yolk Sac Present', 'Oil Globule Only', 'Absorbed')))
 
 ## Expand week numbers to date ranges
 
@@ -76,13 +66,14 @@ ggplot(larval.tl.freq, aes(x = tl.bin, y = n.tl)) +
   geom_bar(stat = "identity", width = 1, color = "black", fill = "gray80") +
   scale_x_continuous(limits = c(5, 26.55), breaks = seq(5, 25, 5), expand = c(0, 0)) +
   scale_y_continuous(limits = c(0, 65), breaks = seq(0, 60, 15), expand = c(0, 0)) +
+  geom_text(aes(label = paste0("n = ", n.week)), x = Inf, y = Inf, hjust = 1.1, vjust = 1.1, size = 3.5) +
   labs(x = "Length Class (mm)", y = "Frequency") +
   theme(panel.grid = element_blank(), panel.background = element_blank(),
         panel.spacing = unit(1, "lines"), axis.line = element_line(),
         axis.text.x = element_text(size = 13),
         axis.text.y = element_text(size = 13),
-        axis.title.y = element_text(size = 25, margin = margin(0, 15, 0, 0)),
-        axis.title.x = element_text(size = 25, margin = margin(15, 0, 0, 0)),
+        axis.title.y = element_text(size = 21, margin = margin(0, 15, 0, 0)),
+        axis.title.x = element_text(size = 21, margin = margin(15, 0, 0, 0)),
         axis.ticks.length = unit(1.25, 'mm'),
         strip.text = element_text(size = 13),
         strip.background = element_blank()) +
@@ -90,25 +81,3 @@ ggplot(larval.tl.freq, aes(x = tl.bin, y = n.tl)) +
 
 ## Save figure
 ggsave("figures/apis_larval_freq_length_weekly.png", dpi = 300, width = 8, height = 10)
-
-## Yolk/Length Frequency
-
-ggplot(larval.yolk.freq, aes(x = tl.bin, y = n.tl, fill = yolk.cond)) +
-  geom_bar(stat = "identity", width = 1, color = "black") +
-  scale_x_continuous(limits = c(5, 26.55), breaks = seq(5, 25, 5), expand = c(0, 0)) +
-  scale_y_continuous(limits = c(0, 110), breaks = seq(0, 110, 10), expand = c(0, 0)) +
-  scale_fill_grey(start = 0.3, end = 0.85) +
-  labs(x = "Length Class (mm)", y = "Frequency", fill = "Yolk-sac Condition") +
-  theme(panel.grid = element_blank(), panel.background = element_blank(),
-        panel.spacing = unit(1, "lines"), axis.line = element_line(),
-        axis.text.x = element_text(size = 18),
-        axis.text.y = element_text(size = 18),
-        axis.title.y = element_text(size = 25, margin = margin(0, 15, 0, 0)),
-        axis.title.x = element_text(size = 25, margin = margin(15, 0, 0, 0)),
-        axis.ticks.length = unit(2, 'mm'), legend.position = c(0.87, 0.9),
-        legend.text = element_text(size = 13), legend.title = element_text(size = 15),
-        plot.margin = unit(c(5, 5, 5, 5), "mm"))
-
-## Save figure
-ggsave("figures/apis_larval_freq_length_yolk.png", dpi = 300, width = 10, height = 10)
-
